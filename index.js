36ee992fc91b42852f86f294761b3a73f7fc79f8
Harsh Upadhyay <115683306+upadhyayharsh2004@users.noreply.github.com>
2025-08-16 01:04:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,169 @@
+const translations = {
+  "hindi": {
+    "welcome": "मेरे वेब पेज पर आपका स्वागत है!",
+    "sign_in": "साइन इन करें",
+    "have_questions": "कोई प्रश्शन है? कॉल करें 000-800-919-1694",
+    "faqs": "सामान्य प्रश्न",
+    "unlimited_movies_shows": "अनगिनत फ़िल्में टीवी, शो और अधिक।",
+    "watch_anywhere_cancel_anytime": "कहीं भी देखें और कभी भी रद्द करें।",
+    "ready_to_watch": "देखने के लिए तैयार हैं? अपनी ईमेल दर्ज करें या अपनी सदस्यता को बनाएं या पुनः प्रारंभ करें।",
+    "email_address": "ईमेल पता",
+    "get_started": "शुरू हो जाओ",
+    "enjoy_on_tv": "अपने टीवी पर आनंद लें।",
+    "watch_on_smart_tvs": "स्मार्ट टीवी, PlayStation, Xbox, Chromecast, Apple TV, ब्लू-रे प्लेयर और अधिक पर देखें।",
+    "download_shows": "अपने शो को ऑफ़लाइन देखने के लिए डाउनलोड करें।",
+    "save_favourites": "अपने पसंदीदा आसानी से सहेजें और हमेशा देखने के लिए कुछ हो।",
+    "watch_everywhere": "हर जगह देखो।",
+    "stream_unlimited": "अपने फोन, टैबलेट, लैपटॉप और टीवी पर असीमित फ़िल्में और टीवी शो स्ट्रीम करें।",
+    "create_profiles": "बच्चों के लिए प्रोफ़ाइल बनाएँ।",
+    "send_children": "अपने मेम्बरशिप के साथ मुफ़्त में उनके लिए एक अंतरिक्ष में उनके पसंदीदा किरदारों के साथ बच्चों को यात्राओं पर भेजें।",
+    "faq_heading": "अक्सर पूछे जाने वाले प्रश्न",
+    "what_is_netflix": "Netflix क्या है?",
+    "what_is_netflix_answer":"Netflix एक स्ट्रीमिंग सेवा है जो हजारों इंटरनेट से जुड़े उपकरणों पर विभिन्न प्रकार के पुरस्कार विजेता टीवी शो, फिल्में, एनीमे, वृत्तचित्र और बहुत कुछ प्रदान करती है। आप जितना चाहें, जब चाहें देख सकते हैं, बिना किसी विज्ञापन के - सभी एक कम मासिक मूल्य के लिए। हमेशा कुछ न कुछ खोजने के लिए होता है, और हर हफ्ते नए टीवी शो और फिल्में जोड़ी जाती हैं!",
+    "how_much_cost": "Netflix का कितना खर्च होता है?",
+    "how_much_cost_answer": "अपने स्मार्टफ़ोन, टैबलेट, स्मार्ट टीवी, लैपटॉप या स्ट्रीमिंग डिवाइस पर Netflix देखें, यह सब एक निश्चित मासिक शुल्क के लिए। योजनाएं ₹199 से ₹799 प्रति माह तक होती हैं। कोई अतिरिक्त लागत नहीं, कोई अनुबंध नहीं।",
+    "where_watch": "मैं कहां देख सकता हूँ?",
+    "where_watch_answer": "किसी भी डिवाइस पर कहीं भी, कभी भी देख सकते हैं। अपने Netflix खाते में साइन इन करें और वेब पर netflix.com पर अपने पर्सनल कंप्यूटर से या किसी भी इंटरनेट-कनेक्टेड डिवाइस पर तुरंत देखें, जिसमें Netflix ऐप हो, जैसे कि स्मार्ट टीवी, स्मार्टफ़ोन, टैबलेट, स्ट्रीमिंग मीडिया प्लेयर और गेम कंसोल। आप iOS, Android या Windows 10 ऐप के साथ अपने पसंदीदा शो डाउनलोड भी कर सकते हैं। डाउनलोड करके आप जब चाहें बिना इंटरनेट कनेक्शन के देख सकते हैं। Netflix को कहीं भी ले जाएं।",
+    "how_cancel": "मैं कैसे रद्द करूं?",
+    "how_cancel_answer": "Netflix में लचीलापन है। कोई भी तंत्रिकाएँ नहीं हैं और कोई भी प्रतिबद्धताएँ नहीं हैं। आप आसानी से अपने खाते को ऑनलाइन दो क्लिक में रद्द कर सकते हैं। कोई रद्दीकरण शुल्क नहीं हैं - आप अपने खाते को कभी भी शुरू या बंद कर सकते हैं।",
+    "what_watch": "मैं Netflix से क्या देख सकता हूं?",
+    "what_watch_answer": "Netflix में विशेष फ़ीचर फ़िल्में, डॉक्यूमेंटरी, टीवी शो, एनीमे, पुरस्कार जीतने वाले Netflix ऑरिजिनल्स और बहुत कुछ की व्यापक पुस्तकालय है। आप जितना चाहें, जब चाहें देख सकते हैं।",
+    "netflix_for_kids": "क्या Netflix बच्चों के लिए अच्छा है?",
+    "netflix_for_kids_answer": "Netflix के सदस्यता में Netflix Kids अनुभव शामिल है, जिससे माता-पिता को नियंत्रण मिलता है जबकि बच्चे अपने खुद के अंतरिक्ष में परिवार के दोस्ताना टीवी शो और फिल्मों का आनंद लेते हैं। बच्चों के प्रोफ़ाइल्स परेंटल कंट्रोल्स के साथ आते हैं, जो आपको उनके देखने वाले सामग्री की परिपक्वता रेटिंग को नियंत्रित करने और उन्हें देखने के लिए विशेष शीर्षकों को ब्लॉक करने में मदद करते हैं।",
+    "ready_to_watch_email": "देखने के लिए तैयार हैं? अपनी ईमेल दर्ज करें या अपनी सदस्यता को बनाएं या पुनः प्रारंभ करें।",
+    "investor_relations": "निवेशक संबंध",
+    "privacy": "गोपनीयता",
+    "speed_test": "स्पीड टेस्ट",
+    "help_centre": "सहायता केंद्र",
+    "jobs": "नौकरियाँ",
+    "cookie_preferences": "कुकी प्रेफ़रेंस",
+    "legal_notices": "कानूनी सूचनाएँ",
+    "account": "खाता",
+    "ways_to_watch": "देखने के तरीके",
+    "corporate_information": "कॉर्पोरेट सूचना",
+    "only_on_netflix": "केवल Netflix पर",
+    "media_centre": "मीडिया केंद्र",
+    "terms_of_use": "उपयोग की शर्तें",
+    "contact_us": "संपर्क करें",
+    "email_address_mobile": "ईमेल या मोबाइल नंबर",
+    "password": "पासवर्ड",
+    "or": "या",
+    "sign_in_code": "साइन-इन कोड का इस्तेमाल करें",
+    "forgot_pass": "पासवर्ड भूल गए?",
+    "remember_me": "याद रखें",
+    "new_to_netflix": "Netflix पर नए हैं?",
+    "sign_up": "अभी साइन अप करें",
+    "captcha": "यह पक्का करने के लिए कि आप बॉट नहीं हैं, यह पेज Google reCAPTCHA द्वारा सुरक्षित है.",
+    "l_m": "और जानें.",
+    "question": "कोई सवाल है? कॉल करें",
+    "call": "000-800-919-1694"
+  },
+};
+
+// Function to toggle accordion and translate content
+function toggleAccordion(element) {
+  const faqContent = element.querySelector('.FAQ__visible');
+  if (faqContent.style.maxHeight) {
+    faqContent.style.maxHeight = null;
+    element.classList.remove("expanded");
+  } else {
+    faqContent.style.maxHeight = faqContent.scrollHeight + "px";
+    element.classList.add("expanded");
+  }
+}
+
+// Add event listener to each accordion
+let accordions = document.querySelectorAll(".FAQ__accordion");
+accordions.forEach(accordion => {
+  accordion.addEventListener("click", function () {
+    toggleAccordion(this);
+  });
+});
+
+function changeLanguage(language) {
+  if (language === "english") {
+    window.location.reload();
+    return;
+  }
+  const elements = document.querySelectorAll("[data-translate]");
+  elements.forEach(element => {
+    const key = element.getAttribute("data-translate");
+    element.textContent = translations[language][key];
+  });
+
+  if (language === "hindi") {
+    const linkRow2 = document.querySelector(".link-row-2 a");
+    if (linkRow2) {
+      linkRow2.style.marginRight = "232px";
+    }
+    const signupLink = document.querySelector(".signup-link");
+    if (signupLink) {
+      signupLink.style.marginLeft = "-73px";
+    }
+  }
+  
+  const languagesSelect = document.getElementById("languagesSelect");
+  if (languagesSelect) {
+    languagesSelect.value = language;
+  }
+  const languagesSelectFooter = document.getElementById("languagesSelectFooter");
+  if (languagesSelectFooter) {
+    languagesSelectFooter.value = language;
+  }
+}
+
+// Change language on page load
+window.onload = function () {
+  const selectLanguage = document.getElementById("languagesSelect");
+  const selectLanguageFooter = document.getElementById("languagesSelectFooter");
+
+  // Set initial language
+  let selectedLanguage = selectLanguage ? selectLanguage.value : (selectLanguageFooter ? selectLanguageFooter.value : 'english');
+
+  if (selectLanguage) {
+    selectLanguage.addEventListener("change", function () {
+      selectedLanguage = selectLanguage.value;
+      changeLanguage(selectedLanguage);
+    });
+  }
+
+  if (selectLanguageFooter) {
+    selectLanguageFooter.addEventListener("change", function () {
+      selectedLanguage = selectLanguageFooter.value;
+      changeLanguage(selectedLanguage);
+    });
+  }
+};
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+  const user_email = formData.get('user_email');
+
+  fetch('/send_email', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user_email }),
+  })
+  .then(response => response.json())
+  .then(data => {
+    if (data.success) {
+      alert(data.message);
+    } else {
+      alert('Error: ' + data.message);
+    }
+  })
+  .catch(error => {
+    alert('Error: ' + error.toString());
+  });
+}
+
+const selectLanguage = document.getElementById('languagesSelect');
+const selectLanguageFooter = document.getElementById('languagesSelectFooter');
+
+if (selectLanguage !== null && selectLanguageFooter !== null) {
+  document.getElementById('emailForm1').addEventListener('submit', handleFormSubmission);
+  document.getElementById('emailForm2').addEventListener('submit', handleFormSubmission);
+}
